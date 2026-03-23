3fad65137150182da4429ad4f76533e0942fac0b
Premchand Pawade <130446286+premchandpawade@users.noreply.github.com>
2026-03-23 16:40:04 +0530

    hotelmanager-service added

---
+++ hotelmanager-service/src/main/java/com/example/hotelmanagerservice/HotelmanagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.hotelmanagerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelmanagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelmanagerServiceApplication.class, args);
+	}
+
+}
---
+++ hotelmanager-service/src/test/java/com/example/hotelmanagerservice/HotelmanagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotelmanagerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelmanagerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
