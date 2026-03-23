378309e46998002396e1bc49c9cf7e842fa9f456
akshaysolanke <akshayashoksolanke@gmail.com>
2026-03-23 23:52:21 +0530

    all setup admin

--- admin-service/src/main/java/com/example/adminservice/models/Admin.java
+++ admin-service/src/main/java/com/example/adminservice/models/Admin.java
@@ -1,4 +1,48 @@
 package com.example.adminservice.models;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int aid;
+    @Column(nullable = false)
+    private String aname;
+    @Column(nullable = false)
+    private String apassword;
+
+    public int getAid() {
+        return aid;
+    }
+
+    public void setAid(int aid) {
+        this.aid = aid;
+    }
+
+    public String getAname() {
+        return aname;
+    }
+
+    public void setAname(String aname) {
+        this.aname = aname;
+    }
+
+    public String getApassword() {
+        return apassword;
+    }
+
+    public void setApassword(String apassword) {
+        this.apassword = apassword;
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "aid=" + aid +
+                ", aname='" + aname + '\'' +
+                ", apassword='" + apassword + '\'' +
+                '}';
+    }
 }
