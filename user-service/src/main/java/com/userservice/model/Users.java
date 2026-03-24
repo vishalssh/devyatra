fd5852ac808e253212ee13ca6c672e30977570d8
Vishal Kushwaha <vishalkushwaha3637@gmail.com>
2026-03-24 20:17:49 +0530

    Refactor Users class to include additional fields and update getter/setter methods

--- user-service/src/main/java/com/userservice/model/Users.java
+++ user-service/src/main/java/com/userservice/model/Users.java
@@ -9,8 +9,22 @@ public class Users {
     private String userId;
     private String firstName;
     private String lastName;
-    private String email;
-    private String mobileNumber;
+    private String userEmail;
+    private String contact;
+    private String gender;
+    private String age;
+    private String userAddress;
+    private String role;
+    private String userPass;
+    private String userStatus;
+
+    public String getUserStatus() {
+        return userStatus;
+    }
+
+    public void setUserStatus(String userStatus) {
+        this.userStatus = userStatus;
+    }
 
     public String getUserId() {
         return userId;
@@ -36,20 +50,60 @@ public class Users {
         this.lastName = lastName;
     }
 
-    public String getEmail() {
-        return email;
+    public String getUserEmail() {
+        return userEmail;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
     }
 
-    public String getMobileNumber() {
-        return mobileNumber;
+    public String getContact() {
+        return contact;
     }
 
-    public void setMobileNumber(String mobileNumber) {
-        this.mobileNumber = mobileNumber;
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getUserAddress() {
+        return userAddress;
+    }
+
+    public void setUserAddress(String userAddress) {
+        this.userAddress = userAddress;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getUserPass() {
+        return userPass;
+    }
+
+    public void setUserPass(String userPass) {
+        this.userPass = userPass;
     }
 
     @Override
@@ -58,8 +112,14 @@ public class Users {
                 "userId='" + userId + '\'' +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
-                ", email='" + email + '\'' +
-                ", mobileNumber='" + mobileNumber + '\'' +
+                ", userEmail='" + userEmail + '\'' +
+                ", contact='" + contact + '\'' +
+                ", gender='" + gender + '\'' +
+                ", age='" + age + '\'' +
+                ", userAddress='" + userAddress + '\'' +
+                ", role='" + role + '\'' +
+                ", userPass='" + userPass + '\'' +
+                ", userStatus='" + userStatus + '\'' +
                 '}';
     }
 }
