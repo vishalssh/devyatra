8eb191a1a02d7245aca1c211aa29903e0b6ccca6
Vishal Kushwaha <vishalkushwaha3637@gmail.com>
2026-03-24 20:17:11 +0530

    implemented the userController and created the APIs

--- user-service/src/main/java/com/userservice/controller/userController.java
+++ user-service/src/main/java/com/userservice/controller/userController.java
@@ -1,9 +1,39 @@
 package com.userservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.userservice.model.Users;
+import com.userservice.repository.userRepo;
+import com.userservice.services.userService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class userController {
+
+    @Autowired
+    userService us;
+
+    @PostMapping("/register")
+    public String userRegister(@RequestBody Users users) {
+        us.saveUser(users);
+        return "User registered successfully: ";
+    }
+
+    @GetMapping("/getUser/{userId}")
+    public String getUser(@PathVariable String userId) {
+        return us.getUser(userId);
+    }
+
+    @GetMapping("/getAllUsers")
+    public List<Users> getAllUsers() {
+        return us.getAllUsers();
+    }
+
+    @PostMapping("/deleteUser/{userId}")
+    public String deleteUser(@PathVariable String userId) {
+        us.deleteUserById(userId);
+        return "User deleted";
+    }
 }
