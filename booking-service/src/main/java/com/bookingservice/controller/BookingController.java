77c01c9c161b196b68f830a43614e13c9d2d5446
Vishal Kushwaha <vishalkushwaha3637@gmail.com>
2026-03-27 22:27:59 +0530

    Add booking retrieval endpoints to BookingController

--- booking-service/src/main/java/com/bookingservice/controller/BookingController.java
+++ booking-service/src/main/java/com/bookingservice/controller/BookingController.java
@@ -4,10 +4,9 @@ package com.bookingservice.controller;
 import com.bookingservice.model.Booking;
 import com.bookingservice.services.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/booking")
@@ -20,6 +19,16 @@ public class BookingController {
     public String createBooking(@RequestBody Booking booking) {
         bookingService.createBooking(booking);
         return "Booking created successfully!";
+    }
+
+    @GetMapping("/getAllBooking")
+    public List<Booking> getAllBookings() {
+        return bookingService.getAllBookings();
+    }
+
+    @GetMapping("/getBooking/{uid}")
+    public String getBooking(@PathVariable String uid) {
+        return bookingService.getBookingById(uid) ;
     }
 
 
