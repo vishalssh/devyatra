278d0f9528830f6978bd3425ed9af2d29f97ccd7
Vishal Kushwaha <vishalkushwaha3637@gmail.com>
2026-04-02 23:10:25 +0530

    feat: add getHotel method to BookingDao for retrieving hotel details by ID

--- booking-service/src/main/java/com/bookingservice/dao/BookingDao.java
+++ booking-service/src/main/java/com/bookingservice/dao/BookingDao.java
@@ -1,17 +1,23 @@
 package com.bookingservice.dao;
 
+import com.bookingservice.dto.HotelDto;
 import com.bookingservice.model.Booking;
 import com.bookingservice.repository.BookingRepo;
 import com.bookingservice.services.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class BookingDao implements BookingService {
     @Autowired
     private BookingRepo bookingRepo;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Override
     public String createBooking(Booking booking) {
@@ -32,6 +38,18 @@ public class BookingDao implements BookingService {
     @Override
     public List<Booking> getBookingByUserId(String userId) {
         return bookingRepo.findByUserId(userId);
+    }
+
+    @Override
+    public Booking getHotel(String hotelId) {
+        String url = "http://localhost:9002/hotel/getHotelById/"+hotelId;
+        HotelDto ht = restTemplate.getForObject(url, HotelDto.class);
+
+        Booking booking = new Booking();
+        if (ht  != null) {
+            booking.getHotelDetails().add(ht);
+        }
+        return booking;
     }
 
 
