890f07a8af7060c03a65c8911ce2d3026a64edcf
Vishal Kushwaha <vishalkushwaha3637@gmail.com>
2026-04-05 22:19:53 +0530

    refactor: simplify Room class by removing getters, setters, and toString method, and adding Lombok @Data annotation

--- hotel-service/src/main/java/com/example/hotelservice/model/Room.java
+++ hotel-service/src/main/java/com/example/hotelservice/model/Room.java
@@ -1,78 +1,21 @@
 package com.example.hotelservice.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Data
 public class Room {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int roomId;
-    private int hotelId;
     private String roomType;
     private int numOfBed;
     private boolean available;
     private double price;
 
-    public int getRoomId() {
-        return roomId;
-    }
+    @ManyToOne
+    @JoinColumn(name = "hotel_id")
+    private Hotel hotel;
 
-    public void setRoomId(int roomId) {
-        this.roomId = roomId;
-    }
-
-    public int getHotelId() {
-        return hotelId;
-    }
-
-    public void setHotelId(int hotelId) {
-        this.hotelId = hotelId;
-    }
-
-    public String getRoomType() {
-        return roomType;
-    }
-
-    public void setRoomType(String roomType) {
-        this.roomType = roomType;
-    }
-
-    public int getNumOfBed() {
-        return numOfBed;
-    }
-
-    public void setNumOfBed(int numOfBed) {
-        this.numOfBed = numOfBed;
-    }
-
-    public boolean isAvailable() {
-        return available;
-    }
-
-    public void setAvailable(boolean available) {
-        this.available = available;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    @Override
-    public String toString() {
-        return "Room{" +
-                "roomId=" + roomId +
-                ", hotelId=" + hotelId +
-                ", roomType='" + roomType + '\'' +
-                ", numOfBed=" + numOfBed +
-                ", available=" + available +
-                ", price=" + price +
-                '}';
-    }
 }
