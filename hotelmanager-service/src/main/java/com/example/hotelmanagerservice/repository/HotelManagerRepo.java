99a5d9197ec46788829fb4d96fdf2b96e25c261b
Vishal Kushwaha <vishalkushwaha3637@gmail.com>
2026-04-08 22:33:33 +0530

    refactor: update Hotelmanager to HotelManager in HotelManagerRepo for consistency

--- hotelmanager-service/src/main/java/com/example/hotelmanagerservice/repository/HotelManagerRepo.java
+++ hotelmanager-service/src/main/java/com/example/hotelmanagerservice/repository/HotelManagerRepo.java
@@ -1,11 +1,10 @@
 package com.example.hotelmanagerservice.repository;
 
-import com.example.hotelmanagerservice.model.Hotelmanager;
+import com.example.hotelmanagerservice.model.HotelManager;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.config.JpaRepositoryNameSpaceHandler;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface HotelManagerRepo extends JpaRepository<Hotelmanager,Integer> {
+public interface HotelManagerRepo extends JpaRepository<HotelManager,Integer> {
 
 }
